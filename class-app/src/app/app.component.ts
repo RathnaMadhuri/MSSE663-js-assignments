fae2cb3193213177e8807a08460beb18e95a6c49
rathnabonigala <rathnabonigala@gmail.com>
2022-01-18 20:48:01 -0700

    updated hello world

--- class-app/src/app/app.component.ts
+++ class-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Hello World!';
+  title = 'Hello World is running';
+  
 }
