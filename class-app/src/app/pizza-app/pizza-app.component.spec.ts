a7877cfc771e9c68b7c884b63562469f470d395e
rathnabonigala <rathnabonigala@gmail.com>
2022-01-25 14:00:42 -0700

    Commit changes to week2

---
+++ class-app/src/app/pizza-app/pizza-app.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PizzaAppComponent } from './pizza-app.component';
+
+describe('PizzaAppComponent', () => {
+  let component: PizzaAppComponent;
+  let fixture: ComponentFixture<PizzaAppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PizzaAppComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PizzaAppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ class-app/src/app/pizza-app/pizza-app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-app',
+  templateUrl: './pizza-app.component.html',
+  styleUrls: ['./pizza-app.component.scss'],
+})
+export class PizzaAppComponent {
+  title = 'PizzaComponent works';
+  prices = {
+    small: { base: 9.99, size: 10 },
+    meduim: { base: 11.99, size: 12 },
+    large: { base: 13.99, size: 14 },
+    'x-large': { base: 15.99, size: 16 },
+  };
+  
+}
